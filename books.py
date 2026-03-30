38457e9e9bb7d628d74f5fda9d2d2c7e5a192074
AlexandraInTravel <alexandraintravel2003@gmail.com>
2026-03-30 23:20:29 +0300

    Add files via upload

--- books.py
+++ books.py
@@ -1,35 +1,35 @@
 #задание 5
-class Book:
-    def __init__(self, title, author, year):
+class Book: #родительский класс
+    def __init__(self, title, author, year): #конструктор
         self.title = title
         self.author = author
         self.year = year
 
-    def info(self): 
+    def info(self): #метод, который выводит информацию о книге
         print('Название книги:', self.title)  
         print('Автор книги:', self.author)
         print('Год выпуска книги:', self.year)
     
-    def is_actual(self):
-        a = 2026 - self.year
-        if a > 5:
+    def is_actual(self): #метод, проверяющий актуальность книги
+        a = 2026 - self.year #будем сравнивать с 2026 годом
+        if a > 5: #если больше 5 лет, то
             print('Книга уже не актуальна')
-        else:
+        else: #в другом случае
             print('Книга актуальна')
 
 
-class EBook(Book):  
-    def __init__(self, title, author, year, file_size, format,free):
+class EBook(Book): #дочерний класс
+    def __init__(self, title, author, year, file_size, format,free): #конструктор
         super().__init__(title, author, year)
         self.file_size = file_size
         self.format = format
         self.free=free
 
-    def info(self):
-        super().info()
+    def info(self): #расширение информации о книге
+        super().info() #наследование из родительского класса в дочерний метода info()
         print('Размер книги:', self.file_size)
         print('Формат книги:', self.format)
-    def is_available(self):
+    def is_available(self): #метод, который проверяет: книга электронная (есть в наличии) или бумажная (нет в наличии)
         if self.format=='электронный':
             print('Есть в наличии')
         elif self.format == 'бумажный':
@@ -37,7 +37,7 @@ class EBook(Book):
         else:
             print('Неверный формат книги')
 
-    def is_free(self):
+    def is_free(self): 
         if self.free == 'да':
             print('Книгу можно получить бесплатно')
         elif self.free == 'нет':
@@ -45,7 +45,7 @@ class EBook(Book):
         else:
             print('Неверный статус получения книги')
 
-    def actual(self):
-        super().is_actual()
+    def actual(self): #доп метод
+        super().is_actual() #наследование 
 
 
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 #задание 5
-from books import EBook
+from books import EBook #импорт класса
 
-def main():
-    title=input('Введите название книги: ')
+def main(): #главная функция
+    title=input('Введите название книги: ') #данные для программы
     author=input('Введите автора книги: ')
     year=int(input('Введите год выпуска книги: '))
     file_size=input('Введите размер книги (количество страниц): ')
     format=input('Книга в электронном или в бумажном варианте? (электронный/бумажный) ')
     free=input('Можно ли получить книгу бесплатно? (да/нет) ')
-    a=EBook(title,author,year, file_size, format, free)
-    a.info()
+    a=EBook(title,author,year, file_size, format, free) #присваивание переменной
+    a.info() #вызов функции класса
     a.actual()
     a.is_available()
     a.is_free()
-if __name__=='__main__':
+if __name__=='__main__': 
     main()
