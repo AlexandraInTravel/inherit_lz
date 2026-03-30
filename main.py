220ad45311c5170a851089f38561da09d5251315
AlexandraInTravel <alexandraintravel2003@gmail.com>
2026-03-30 21:11:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from books import EBook
+
+def main():
+    title=input('Введите название книги: ')
+    author=input('Введите автора книги: ')
+    year=int(input('Введите год выпуска книги: '))
+    file_size=input('Введите размер книги (количество страниц): ')
+    format=input('Книга в электронном или в бумажном варианте? ')
+    free=input('Можно ли получить книгу бесплатно? ')
+    a=EBook(title,author,year, file_size, format, free)
+    a.info()
+    a.actual()
+    a.is_available()
+    a.is_free()
+if __name__=='__main__':
+    main()
